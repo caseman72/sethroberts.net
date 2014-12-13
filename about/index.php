bd13c98f937f90a681c6a2e4389addf6043b5c37
twoutopias <amyrogers@Amys-iMac.local>
2014-12-13 12:40:19 -0800

    Added date of birth and death

--- about/index.php
+++ about/index.php
@@ -35,7 +35,7 @@
       <div id="mainBody">
         <h1>About the Author</h1>
         <img class="bookFloatLeft" src="/images/sethrobertsPicBox.jpg" alt="Seth Roberts"/>
-        <p>Seth Roberts, Ph.D., is a professor of psychology at <a href="http://www.tsinghua.edu.cn/eng/index.jsp">Tsinghua University</a> in Beijing and a professor emeritus of psychology at the University of California at Berkeley.
+        <p>Seth Roberts, Ph.D., (1954 - 2014) was a professor of psychology at <a href="http://www.tsinghua.edu.cn/eng/index.jsp">Tsinghua University</a> in Beijing and a professor emeritus of psychology at the University of California at Berkeley.
         </p>
         <div id="pullBoxRight">
           <h2>More about<br/>the author</h2>
